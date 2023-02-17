a3dcf7036e2730656fb654a7e0f309a8809cf5ff
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2023-02-17 16:31:09 +0900

    Working minimal example

--- Help/guide/tutorial/Step1/tutorial.cxx
+++ Help/guide/tutorial/Step1/tutorial.cxx
@@ -1,29 +1,10 @@
-// A simple program that computes the square root of a number
-#include <cmath>
 #include <cstdlib> 
 #include <iostream>
 #include <string>
 #include "TutorialConfig.h"
 
-// TODO 11: Include TutorialConfig.h
-
-int main(int argc, char* argv[])
+int main()
 {
-  if (argc < 2) {
-    // TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
-    //          and Tutorial_VERSION_MINOR
-    std::cout << "Version: " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
-    return 1;
-  }
-
-  // convert input to double
-  // TODO 4: Replace atof(argv[1]) with std::stod(argv[1])
-  const double inputValue = std::stod(argv[1]);
-
-  // calculate square root
-  const double outputValue = sqrt(inputValue);
-  std::cout << "The square root of " << inputValue << " is " << outputValue
-            << std::endl;
+  std::cout << "Version: " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
   return 0;
 }
